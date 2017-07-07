7d04aaa62d02c6e9613f02db8f92439d250dd64f
Zhen <zhl146@gmail.com>
2017-07-07 17:13:53 -0400

    building leaderboard connection to backend

--- src/LeaderBoardView.js
+++ src/LeaderBoardView.js
@@ -14,10 +14,28 @@ LeaderBoardView.prototype.initLeaderBoard = function() {
 };
 
 LeaderBoardView.prototype.populateLeaderBoard = function() {
-
+    this.getRemoteScores()
 };
 
 LeaderBoardView.prototype.getRemoteScores = function() {
+    var startIndex = 0;
+    var numScores = 10;
+
+    var xhr = new XMLHttpRequest();
+    // this is the maze checking URL
+    var url = 'http://localhost:3000/leaderboard?begin=' + startIndex + '&' + '?num=' + numScores;
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            // NEED TO EDIT TO RECEIVE THINGS FROM SERVER
+            this.addScoresToLeaderboard(json.scores);
+        }
+    }.bind(this);
+    xhr.send(null);
+};
+
+LeaderBoardView.prototype.addScoresToLeaderboard = function(scores) {
 
 };
 
