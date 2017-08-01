ceff4c40368dd4fa18ddc598cd34fcb960728c0b
Zhen <zhl146@gmail.com>
2017-08-01 15:15:22 -0400

    got rid of jasmine and karma using tape instead

--- server/tests/shared/point.test.js
+++ server/tests/shared/point.test.js
@@ -1,7 +1,6 @@
 "use strict";
 let test = require('tape');
 let Point = require('../../../shared/Point');
-//let assert = require('assert');
 
 class PointTest {
     constructor(){
@@ -9,43 +8,13 @@ class PointTest {
     }
 
     unitTests(){
-        test('Point Test', function(assert){
+        test('the point should report the correct x and y coordinates', function(assert){
             let samplePoint = new Point.default(10, 10);
             assert.equal(samplePoint.x, 10);
+            assert.equal(samplePoint.y, 10);
             assert.end();
         });
-
-        test('this test should fail', (assert) => {
-                assert.equal(false, true);
-                assert.end();
-            }
-        );
     }
 }
 
 module.exports = PointTest;
-
-
-// describe('Point', function() {
-
-//     let samplePoint;
-
-//     beforeEach(() => {
-//         samplePoint = new Point(10, 10)
-//     });
-
-//     describe('when a point is created', () => {
-//         it('should be able to report its coordinates', () => {
-//             expect(samplePoint.x).toEqual(11);
-//         });
-//     });
-
-//     describe('this test should fail', () => {
-//         it('should fail', () => {
-//                 expect(false).toBe(true);
-//             }
-//         )
-
-//     })
-
-// });
--- test-context.js
+++
@@ -1,2 +0,0 @@
-var context = require.context('./spec/shared', true, /.spec\.js$/);
-context.keys().forEach(context);
