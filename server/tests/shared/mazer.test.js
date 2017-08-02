8827be53be1a44bacc533a710ed537e1a8d68fc4
pmsabat <pasab75@gmail.com>
2017-08-02 17:29:37 +0100

    adding mazer test stub

---
+++ server/tests/shared/mazer.test.js
@@ -0,0 +1,20 @@
+"use strict";
+let test = require('tape');
+let Point = require('../../../shared/Point');
+
+class MazerTest {
+    constructor(){
+
+    }
+
+    mazerConstructorTest(assert){
+
+    }
+    
+    unitTests(){
+        test('testing the constructor for mazer', this.mazerConstructorTest);
+        
+    }
+}
+
+module.exports = MazerTest;
