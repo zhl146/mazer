061eec228aec8c18496a9b894371ed1ceca571e5
pmsabat <pasab75@gmail.com>
2017-08-02 17:55:42 +0100

    changing point to be a class so tile can extend, updating maze test

--- server/tests/shared/mazer.test.js
+++ server/tests/shared/mazer.test.js
@@ -1,6 +1,6 @@
 "use strict";
 let test = require('tape');
-let Point = require('../../../shared/Point');
+let Mazer = require('../../../shared/Mazer');
 
 class MazerTest {
     constructor(){
@@ -8,9 +8,13 @@ class MazerTest {
     }
 
     mazerConstructorTest(assert){
-
+        let testMaze1 = new Mazer.default(3000);
+        let testMaze2 = new Mazer.default(3000);
+        assert.equal(testMaze1.random != null, true);
+        assert.equal(testMaze1.random == testMaze2.random, true);
+        assert.end();
     }
-    
+
     unitTests(){
         test('testing the constructor for mazer', this.mazerConstructorTest);
         
--- shared/Point.js
+++ shared/Point.js
@@ -1,73 +1,45 @@
 "use strict";
-export default function Point(x, y) {
-    this.x = x;
-    this.y = y;
-    this.parent = null;
-};
 
-Point.prototype.calculateDistance = function ( point ) {
-    const xDiff = this.x - point.x;
-    const yDiff = this.y - point.y;
-    return Math.sqrt(xDiff*xDiff + yDiff*yDiff);
-};
+class Point{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.parent = null;
+        this.g = null;
+    }
 
-Point.prototype.copy = function() {
-  return new Point(this.x, this.y);
-};
+    calculateDistance( point ) {
+        const xDiff = this.x - point.x;
+        const yDiff = this.y - point.y;
+        return Math.sqrt(xDiff*xDiff + yDiff*yDiff);
+    }
 
-Point.prototype.matches = function ( pointToCompare ) {
-    return ( this.x === pointToCompare.x && this.y === pointToCompare.y );
-};
+    copy() {
+        return new Point(this.x, this.y);
+    }
 
-Point.prototype.setParent = function(parentPoint) {
-    this.parent = parentPoint;
-};
+    matches(pointToCompare) {
+        return ( this.x === pointToCompare.x && this.y === pointToCompare.y );
+    }
 
-Point.prototype.setG = function(cost) {
-    this.g = this.parent.g + cost;
-};
+    setParent(parentPoint) {
+        this.parent = parentPoint;
+    }
 
-Point.prototype.setH = function(endPoint) {
-    const xDiff = Math.abs(endPoint.x - this.x);
-    const yDiff = Math.abs(endPoint.y - this.y);
-    this.h = 7 * (xDiff + yDiff);
-};
+    setG(cost) {
+        this.g = this.parent.g + cost;
+    }
+    
+    setH(endPoint) {
+        const xDiff = Math.abs(endPoint.x - this.x);
+        const yDiff = Math.abs(endPoint.y - this.y);
+        this.h = 7 * (xDiff + yDiff);
+    }
 
-Point.prototype.setF = function() {
-    this.f = this.g + this.h;
-};
+    setF() {
+        this.f = this.g + this.h;
+    }
 
-Point.prototype.getAdjacent = function(maze, endpoint) {
-    const pointArray = [];
-    let newPoint;
-    const self = this;
+}
 
-    const addPoint = function ( newPoint, isDiagonal ) {
-        if (maze.isPassable(newPoint)) {
-            newPoint.setParent(self);
-            newPoint.setG(isDiagonal ? 14 : 10);
-            newPoint.setH(endpoint);
-            newPoint.setF();
-            pointArray.push(newPoint)
-        }
-    };
-
-    newPoint = new Point( this.x, this.y + 1 );
-    addPoint(newPoint, false);
-    newPoint = new Point( this.x + 1, this.y + 1 );
-    addPoint(newPoint, true);
-    newPoint = new Point( this.x + 1, this.y );
-    addPoint(newPoint, false);
-    newPoint = new Point( this.x + 1, this.y - 1 );
-    addPoint(newPoint, true);
-    newPoint = new Point( this.x, this.y - 1 );
-    addPoint(newPoint, false);
-    newPoint = new Point( this.x - 1, this.y - 1 );
-    addPoint(newPoint, true);
-    newPoint = new Point( this.x - 1, this.y );
-    addPoint(newPoint, false);
-    newPoint = new Point( this.x - 1, this.y + 1 );
-    addPoint(newPoint, true);
-
-    return pointArray;
-};
+module.exports = Point;
