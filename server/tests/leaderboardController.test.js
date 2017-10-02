5b0df3c6c99640d18ea7e873cec12f09a5dbcf3f
pmsabat <pasab75@gmail.com>
2017-10-02 22:15:36 +0100

    updating with unit tests

--- server/tests/leaderboardController.test.js
+++ server/tests/leaderboardController.test.js
@@ -1,6 +1,5 @@
 import leaderboardController from "../controllers/leaderboardController";
 import test from "tape-async";
-import sleep from "sleep-promise";
 import ScoreModel from "../database/ScoreModel";
 import mongoose from "mongoose";
 
@@ -14,13 +13,20 @@ const setup = async () => {
         scoremodel.solution = JSON.stringify([]);
         let savedScore = await scoremodel.save();
     }
-    let scores = await leaderboardController(0, "testseed", 3); //leaderboardController(start, seed, length)
+    let scores = await leaderboardController(0, "testseed", 10); //leaderboardController(start, seed, length)
     let scores2 = await leaderboardController(1, "testseed", 4); //leaderboardController(start, seed, length)
     console.log("derpderp");
     return {
         scores,
         scores2
     };
+};
+
+const teardown = async scores => {
+    scores.forEach(score => {
+        let scoremodel = new ScoreModel();
+        scoremodel.remove({ email: score.email });
+    });
 };
 
 test("the leaderboard controller should successfully fetch scores", async assert => {
@@ -30,7 +36,7 @@ test("the leaderboard controller should successfully fetch scores", async assert
 
     const { scores, scores2 } = await setup();
 
-    assert.deepEqual(scores.length, 3);
+    assert.deepEqual(scores.length, 10);
     assert.deepEqual(scores[0].score, 9);
     assert.deepEqual(scores[1].score, 8);
     assert.deepEqual(scores[2].score, 7);
@@ -40,4 +46,6 @@ test("the leaderboard controller should successfully fetch scores", async assert
     assert.deepEqual(scores[1].score, 7);
     assert.deepEqual(scores[2].score, 6);
     assert.deepEqual(scores[2].score, 5);
+
+    await teardown(scores);
 });
