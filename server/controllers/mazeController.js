45e65f8f95ffb23955f15385d1576d6c2db85550
zhen lu <zhen_lu@comcast.com>
2018-07-04 20:48:11 -0400

    prettier

--- server/controllers/mazeController.js
+++ server/controllers/mazeController.js
@@ -1,49 +1,44 @@
-import SeedUtil from "./maze-functions/generate-seed";
-import { createMaze } from "mazer-shared";
-import mongo from "../mongodb";
-import getIdFromToken from "./getIdFromToken";
+import SeedUtil from './maze-functions/generate-seed'
+import { createMaze } from 'mazer-shared'
+import mongo from '../mongodb'
+import getIdFromToken from './getIdFromToken'
 
 const submitSolution = async submission => {
-    let baseMaze = createMaze(submission.seed);
-    let valid = baseMaze.applyUserChanges(submission.solution);
-    let score = baseMaze.score;
-    if (!valid) return { error: true, status: "u r a cheat!" };
-    // First search for duplicates
+  let baseMaze = createMaze(submission.seed)
+  let valid = baseMaze.applyUserChanges(submission.solution)
+  let score = baseMaze.score
+  if (!valid) return { error: true, status: 'u r a cheat!' }
+  // First search for duplicates
 
-    const userId = await getIdFromToken(submission.token);
+  const userId = await getIdFromToken(submission.token)
 
-    let matchingScore = await mongo.scores.findOne({
-        seed: submission.seed,
-        userId
-    });
+  let matchingScore = await mongo.scores.findOne({
+    seed: submission.seed,
+    userId,
+  })
 
-    console.log("matching scores", matchingScore);
+  console.log('matching scores', matchingScore)
 
-    const newScore = {
-        name: submission.name,
-        userId,
-        score,
-        seed: submission.seed,
-        solution: submission.solution
-    };
+  const newScore = {
+    name: submission.name,
+    userId,
+    score,
+    seed: submission.seed,
+    solution: submission.solution,
+  }
 
-    console.log(newScore);
+  if (!matchingScore) await mongo.scores.insertOne(newScore)
+  else if (matchingScore.score <= score)
+    await mongo.scores.updateOne({ _id: matchingScore._id }, { $set: newScore })
 
-    if (!matchingScore) await mongo.scores.insertOne(newScore);
-    else if (matchingScore.score <= score)
-        await mongo.scores.updateOne(
-            { _id: matchingScore._id },
-            { $set: newScore }
-        );
-
-    return { error: false, status: "success" };
-};
+  return { error: false, status: 'success' }
+}
 
 const getDailySeed = () => {
-    return SeedUtil.dateToSeed(new Date());
-};
+  return SeedUtil.dateToSeed(new Date())
+}
 
 export default {
-    POST: submitSolution,
-    GET: getDailySeed
-};
+  POST: submitSolution,
+  GET: getDailySeed,
+}
