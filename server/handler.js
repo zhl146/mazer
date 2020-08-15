c7ec48cb0dc522145f0115beca5b499be1e1132e
zhl146 <zhl146@gmail.com>
2020-08-15 16:33:19 -0400

    add mongodb

--- server/handler.js
+++ server/handler.js
@@ -1,7 +1,34 @@
-module.exports.test = async (event) => {
+const MongoClient = require('mongodb').MongoClient
+
+const { MONGO_URL, MONGO_USER, MONGO_PASSWORD } = process.env
+
+const auth = {
+  user: MONGO_USER,
+  password: MONGO_PASSWORD,
+}
+
+const getConnectionPool = async () => {
+  let cachedPool
+
+  if (cachedPool) return cachedPool
+
+  const pool = MongoClient.connect(MONGO_URL, { auth })
+  cachedPool = pool
+  return pool
+}
+
+module.exports.test = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+
+  const mongoPool = await getConnectionPool()
+
+  const result = await mongoPool
+    .db('sample_airbnb')
+    .collection('listingsAndReviews')
+    .findOne()
 
   return {
     statusCode: '200',
-    body: 'Hello World'
-  };
-};
+    body: result,
+  }
+}
