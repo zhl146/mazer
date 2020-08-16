069cb87e19fe7dd7f26cc4602d2be86ad3d9ac3e
zhl146 <zhl146@gmail.com>
2020-08-15 21:27:48 -0400

    fmove around serverless files

--- server/handler.js
+++ server/handler.js
@@ -1,39 +1,39 @@
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 
-const { MONGO_URL, MONGO_USER, MONGO_PASSWORD } = process.env
+const { MONGO_URL, MONGO_USER, MONGO_PASSWORD } = process.env;
 
 const auth = {
   user: MONGO_USER,
   password: MONGO_PASSWORD,
-}
+};
 
 const getConnectionPool = async () => {
-  let cachedPool
+  let cachedPool;
 
-  if (cachedPool) return cachedPool
+  if (cachedPool) return cachedPool;
 
   const pool = MongoClient.connect(MONGO_URL, {
     auth,
     useUnifiedTopology: true,
-  })
-  cachedPool = pool
-  return pool
-}
+  });
+  cachedPool = pool;
+  return pool;
+};
 
 const testHandler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  const mongoPool = await getConnectionPool()
+  const mongoPool = await getConnectionPool();
 
   const result = await mongoPool
-    .db('sample_mflix')
-    .collection('movies')
-    .findOne()
+    .db("sample_mflix")
+    .collection("movies")
+    .findOne();
 
   return {
-    statusCode: '200',
+    statusCode: "200",
     body: result,
-  }
-}
+  };
+};
 
-module.exports.test = testHandler
+module.exports.test = testHandler;
