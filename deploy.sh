ed8a82df608e50382792d9822a211d2d8f0fccf4
zhl146 <zhl146@gmail.com>
2020-08-16 11:52:58 -0400

    use package.json

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-npm install -g serverless
-npm install -g serverless-bundle
-serverless deploy --stage $env --package \
+npm install -g yarn
+yarn install
+npx serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
