26b71a41e5ae993c17633c7b4a6614d26e17a66a
zhl146 <zhl146@gmail.com>
2020-08-16 14:38:49 -0400

    test mongodb

--- server/handler.js
+++ server/handler.js
@@ -1,43 +1,40 @@
-// const MongoClient = require("mongodb").MongoClient;
-import { createGame } from "@mazer/shared";
+const MongoClient = require("mongodb").MongoClient;
 
-const { MONGO_URL, MONGO_USER } = process.env;
+const { MONGO_URL, MONGO_USER, MONGO_PASSWORD } = process.env;
 
-// const auth = {
-//   user: MONGO_USER,
-//   password: MONGO_PASSWORD,
-// };
+const auth = {
+  user: MONGO_USER,
+  password: MONGO_PASSWORD,
+};
 
-// let cachedPool;
+let cachedPool;
 
-// const getConnectionPool = async () => {
-//   if (cachedPool) return cachedPool;
+const getConnectionPool = async () => {
+  if (cachedPool) return cachedPool;
 
-//   const pool = MongoClient.connect(MONGO_URL, {
-//     auth,
-//     useUnifiedTopology: true,
-//   });
-//   cachedPool = pool;
-//   return pool;
-// };
+  const pool = await MongoClient.connect(MONGO_URL, {
+    auth,
+    useUnifiedTopology: true,
+  });
+
+  cachedPool = pool;
+  return pool;
+};
 
 export const test = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  console.log(MONGO_URL, MONGO_USER);
-
-  const newGame = createGame("derp");
 
   console.log(newGame);
 
-  // const mongoPool = await getConnectionPool();
+  const mongoPool = await getConnectionPool();
 
-  // const result = await mongoPool
-  //   .db("sample_mflix")
-  //   .collection("movies")
-  //   .findOne();
+  const result = await mongoPool
+    .db("sample_mflix")
+    .collection("movies")
+    .findOne();
 
   return {
     statusCode: "200",
-    body: newGame,
+    body: result,
   };
 };
