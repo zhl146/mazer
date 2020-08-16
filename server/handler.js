d60b2d9686525e503cb9e60a9160616d51f06502
zhl146 <zhl146@gmail.com>
2020-08-16 14:27:08 -0400

    fix linting issue

--- server/handler.js
+++ server/handler.js
@@ -1,4 +1,4 @@
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
 import { createGame } from "@mazer/shared";
 
 const { MONGO_URL, MONGO_USER } = process.env;
