fb1d800ebf0f9052a0c247a1897c56a753348bee
zhl146 <zhl146@gmail.com>
2020-08-24 08:25:53 -0400

    add google auth

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,4 +1,7 @@
 import { useState, useRef } from "react";
+import { useGoogleLogin, useGoogleLogout } from "react-google-login";
+
+import { GOOGLE_CLIENT_ID } from "@mazer/shared";
 
 export const usePathError = (errorDuration) => {
   const [error, setError] = useState(false);
@@ -16,4 +19,26 @@ export const usePathError = (errorDuration) => {
   };
 
   return [error, startErrorState];
+};
+
+export const useGoogleOauth = () => {
+  const [userToken, setUserToken] = useState(null);
+
+  const { signIn, loaded } = useGoogleLogin({
+    onSuccess: ({ tokenId }) => setUserToken(tokenId),
+    clientId: GOOGLE_CLIENT_ID,
+    onFailure: (data) => console.log(data),
+  });
+
+  const { signOut } = useGoogleLogout({
+    onLogoutSuccess: () => setUserToken(null),
+    clientId: GOOGLE_CLIENT_ID,
+    onFailure: (data) => console.log(data),
+  });
+
+  return {
+    userToken,
+    signIn,
+    signOut,
+  };
 };
